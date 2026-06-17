f0fc5ee90a707bae78434c95c7ef07c1bee15c0c
techCoderRohit <rohitkumarmodi77@gmail.com>
2026-06-17 12:40:30 +0530

    first commit

---
+++ Practice Tasks/Basic.py
@@ -0,0 +1,34 @@
+print('Mera pehla python code')
+
+name = 'Rahul'
+age = 20
+city = 'Lucknow'
+is_student = True
+
+print("Name:", name, " | Age:",age, " | City:",city, " | Is student?:",is_student)
+
+user_name =input("Enter your name: ")
+print("Hello " + user_name)
+
+num1 = int(input('Enter num1: '))
+num2 = int(input('Enter num2: '))
+sum = num1 + num2
+print("sum: ", sum)
+
+pi = 3.14
+print(type(pi))
+
+a = 5
+b = 8
+a , b = b , a
+print(a, b)
+
+num = int(input("Enter number: "))
+square = num * num
+print(square)
+
+s1 = "Amar"
+s2 = "Amit"
+s3 = s1 + s2
+print(s3)
+
